b28116528ad249b4aa1f906274b7661a3773f65b
Marcelo Brito <marcelobrito@marcelobrito.net>
2020-03-23 00:10:59 -0300

    Updates

---
+++ precache-manifest.26519f5f1afb37ead2a7ba60d49b4e44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f8cf7a69db08e1daf54a161c4841db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbde47c69680af4e601c",
+    "url": "/static/js/2.9bf724e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9bf724e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06409db0e3fb1a9fe21c",
+    "url": "/static/js/main.87fb4879.chunk.js"
+  },
+  {
+    "revision": "e5bb0d80a2c79e9fbd24",
+    "url": "/static/js/runtime-main.0948fd9d.js"
+  }
+]);
